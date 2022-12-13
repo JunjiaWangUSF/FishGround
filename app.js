888c889cf0cdd9521c4d2662a97877837e676428
JunjiaWangUSF <jwang268@dons.usfca.edu>
2022-12-13 01:41:23 -0800

    show page add router

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const Review = require("./models/review");
 //     console.log("MIDDLEWARE to handle expections")
 // })
 
+const fishgrounds = require("./routes/fishground");
+
 const FishGround = require("./models/FishGround");
 const Override = require("method-override"); //Overide-let moogse to delete and update
 mongoose
@@ -36,7 +38,7 @@ app.set("view engine", "ejs"); //set dynamic html scripts
 app.set("views", path.join(__dirname, "views")); // make view
 app.use(express.urlencoded({ extended: true }));
 app.use(Override("_method"));
-
+app.use("/fishground", fishgrounds);
 app.get("/", (req, res) => {
   res.render("home");
 });
@@ -48,60 +50,6 @@ app.get("/makefishground", async (req, res) => {
   });
   await fisher.save(); //wait to save to into Database and then send out responds.
   res.send(fisher);
-});
-
-app.get("/fishground", async (req, res) => {
-  const titleOfIndex = await FishGround.find({});
-  //console.log(index);
-  res.render("fishgrounds/index", { titleOfIndex });
-});
-
-app.get("/fishground/new", (req, res) => {
-  res.render("fishgrounds/new");
-});
-
-app.get("/fishground/loginAndRegister", (req, res) => {
-  res.render("fishgrounds/loginAndRegisteration");
-});
-
-app.post("/fishground", async (req, res) => {
-  //res.send(req.body)
-  const fishground = new FishGround(req.body.fishground);
-  //confirm.log(fishground)
-  await fishground.save();
-  res.redirect(`/fishground/${fishground._id}`);
-});
-
-//21 22 27 33 36
-app.get("/fishground/:id", async (req, res) => {
-  const fishground = await FishGround.findById(req.params.id).populate(
-    "reviews"
-  );
-  console.log(fishground);
-  res.render("fishgrounds/show", { fishground });
-});
-
-app.get("/fishground/:id/edit", async (req, res) => {
-  const fishground = await FishGround.findById(req.params.id);
-  res.render("fishgrounds/edit", { fishground });
-});
-
-app.put("/fishground/:id", async (req, res) => {
-  //post request
-  //res.send("It works fine")
-  const { id } = req.params;
-  const fishground = await FishGround.findByIdAndUpdate(id, {
-    ...req.body.fishground,
-  });
-  res.redirect(`/fishground/${fishground._id}`);
-});
-
-app.delete("/fishground/:id", async (req, res) => {
-  //post request
-  //res.send("It works fine")
-  const { id } = req.params;
-  const fishground = await FishGround.findByIdAndDelete(id);
-  res.redirect(`/fishground`);
 });
 
 app.delete("/fishground/:id/reviews/:reviewId", async (req, res) => {
---
+++ routes/fishground.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const router = express.Router();
+const FishGround = require("../models/FishGround");
+const Review = require("../models/review");
+
+router.get("/", async (req, res) => {
+  const titleOfIndex = await FishGround.find({});
+  //console.log(index);
+  res.render("fishgrounds/index", { titleOfIndex });
+});
+
+router.get("/new", (req, res) => {
+  res.render("fishgrounds/new");
+});
+
+router.post("/", async (req, res) => {
+  //res.send(req.body)
+  const fishground = new FishGround(req.body.fishground);
+  //confirm.log(fishground)
+  await fishground.save();
+  res.redirect(`/fishground/${fishground._id}`);
+});
+
+//21 22 27 33 36
+router.get("/:id", async (req, res) => {
+  const fishground = await FishGround.findById(req.params.id).populate(
+    "reviews"
+  );
+  console.log(fishground);
+  res.render("fishgrounds/show", { fishground });
+});
+
+router.get("/:id/edit", async (req, res) => {
+  const fishground = await FishGround.findById(req.params.id);
+  res.render("fishgrounds/edit", { fishground });
+});
+
+router.put("/:id", async (req, res) => {
+  //post request
+  //res.send("It works fine")
+  const { id } = req.params;
+  const fishground = await FishGround.findByIdAndUpdate(id, {
+    ...req.body.fishground,
+  });
+  res.redirect(`/fishground/${fishground._id}`);
+});
+
+router.delete("/:id", async (req, res) => {
+  //post request
+  //res.send("It works fine")
+  const { id } = req.params;
+  const fishground = await FishGround.findByIdAndDelete(id);
+  res.redirect(`/fishground`);
+});
+
+module.exports = router;
